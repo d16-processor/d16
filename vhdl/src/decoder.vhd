b7a0cf8c0d6617ff2256f9fec77b6b84f4aecfe1
C-Elegans <mtnolan2640@gmail.com>
2016-06-20 11:49:29 -0400

    Mostly finished decoder, add decoder testbench

--- vhdl/src/decoder.vhd
+++ vhdl/src/decoder.vhd
@@ -4,52 +4,51 @@ use IEEE.numeric_std.ALL;
 use work.cpu_constants.ALL;
 entity decoder is
 	port(
-		clk			: in std_logic;
-		en			: in std_logic;
-		instruction	: in std_logic_vector(15 downto 0);
-		alu_control	: out std_logic_vector(6 downto 0);
-		rD_sel		: out std_logic_vector(2 downto 0);
-		rS_sel		: out std_logic_vector(2 downto 0);
-		immediate	: out std_logic_vector(15 downto 0);
+		clk          : in  std_logic;
+		en           : in  std_logic;
+		instruction  : in  std_logic_vector(15 downto 0);
+		alu_control  : out std_logic_vector(6 downto 0);
+		rD_sel       : out std_logic_vector(2 downto 0);
+		rS_sel       : out std_logic_vector(2 downto 0);
+		immediate    : out std_logic_vector(15 downto 0);
 		en_immediate : out std_logic;
-		next_word	: out std_logic
+		next_word    : out std_logic
 	);
-	
+
 end decoder;
 architecture behavior of decoder is
-variable cycle : std_logic  := '0';
-
-
+	signal s_alu_control 	: std_logic_vector(6 downto 0);
+	signal s_rD_sel 		: std_logic_vector(2 downto 0);
+	signal s_rS_sel			: std_logic_vector(2 downto 0);
+	signal s_immediate		: std_logic_vector(15 downto 0);
+	signal s_en_imm			: std_logic;
+	signal s_next_word		: std_logic;
 begin
-	
-	process(clk,en)
+	alu_control  <= s_alu_control;
+	rD_sel  <= s_rD_sel;
+	rS_sel  <= s_rS_sel;
+	immediate  <= s_immediate;
+	en_immediate  <= s_en_imm;
+	next_word <= s_next_word;
+	process(clk, en)
 	begin
 		if rising_edge(clk) and en = '1' then
-			if rising_edge(en) then
-				cycle  := '0';
-				if unsigned(instruction(15 downto 8)) <= unsigned(OPC_MOVB_R7) and unsigned(instruction(15 downto 8)) >= unsigned(OPC_MOVB_R0) then
-					en_immediate <= '1';
-					alu_control <= OPC_MOV(6 downto 0);
-					next_word <= '0';
-					rD_sel <= "000";
-					immediate <= instruction(7 downto 0);
-				else
-					en_immediate <= instruction(15);
-					next_word <= instruction(15);
-					rD_sel <= instruction(2 downto 0);
-					rS_sel  <= instruction(5 downto 3);
-					immediate  <= X"0000";
-				end if;	
-					
-						
-				
+			if unsigned(instruction(15 downto 8)) <= unsigned(OPC_MOVB_R7) and unsigned(instruction(15 downto 8)) >= unsigned(OPC_MOVB_R0) then
+				s_en_imm <= '1';
+				s_alu_control  <= OPC_MOV(6 downto 0);
+				s_next_word    <= '0';
+				s_rD_sel       <= "000";
+				s_immediate    <= X"00" & instruction(7 downto 0);
 			else
-				immediate <= instruction;
-				cycle := '1';
+				s_alu_control  <= instruction(14 downto 8);
+				s_en_imm <= instruction(15);
+				s_next_word    <= instruction(15);
+				s_rD_sel       <= instruction(2 downto 0);
+				s_rS_sel       <= instruction(5 downto 3);
+				s_immediate    <= X"0000";
 			end if;
-	
-	end if;
-end process;
 
-	
+		end if;
+	end process;
+
 end behavior;
---
+++ vhdl/tests/decoder_tb.vhd
@@ -0,0 +1,66 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+use std.textio.all;
+entity decoder_tb is
+end decoder_tb;
+
+architecture behavior of decoder_tb is
+	component decoder
+		port(
+			clk          : in  std_logic;
+			en           : in  std_logic;
+			instruction  : in  std_logic_vector(15 downto 0);
+			alu_control  : out std_logic_vector(6 downto 0);
+			rD_sel       : out std_logic_vector(2 downto 0);
+			rS_sel       : out std_logic_vector(2 downto 0);
+			immediate    : out std_logic_vector(15 downto 0);
+			en_immediate : out std_logic;
+			next_word    : out std_logic
+		);
+	end component decoder;
+	signal clk 		: std_logic  := '0';
+	signal en 		: std_logic  := '0';
+	signal instruction : std_logic_vector(15 downto 0)  := (others  => '0');
+	signal alu_control : std_logic_vector(6 downto 0)  :=  (others  => '0');
+	signal rD_sel		: std_logic_vector(2 downto 0)  := (others  => '0');
+	signal rS_sel		: std_logic_vector(2 downto 0)  := (others  => '0');
+	signal immediate	: std_logic_vector(15 downto 0)  := (others  => '0');
+	signal en_imm		: std_logic  := '0';
+	signal next_word	: std_logic  := '0';
+	constant clk_period : time  := 10 ns;
+begin
+	uut: component decoder
+		port map(
+			clk          => clk,
+			en           => en,
+			instruction  => instruction,
+			alu_control  => alu_control,
+			rD_sel       => rD_sel,
+			rS_sel       => rS_sel,
+			immediate    => immediate,
+			en_immediate => en_imm,
+			next_word    => next_word
+		);
+	clk_process : process
+		begin
+		clk <= '0';
+        wait for clk_period/2;
+        clk <= '1';
+        wait for clk_period/2;
+	end process;
+	stim_process : process
+		begin
+		wait for 2*clk_period;
+		en <= '1';
+		instruction  <= X"0108";
+		wait for clk_period;
+		instruction  <= X"8100";
+		wait for clk_period;
+		instruction  <= X"0f2b";
+		wait for clk_period;
+		instruction  <= X"0517";
+		wait;
+	end process;
+end behavior;
+
