050856d54a2c6b8c0ac703542306a3edded4e43d
C-Elegans <mtnolan2640@gmail.com>
2016-06-27 20:30:47 -0400

    Fix invalid format string specifier

--- assembler/d16-asm/main.c
+++ assembler/d16-asm/main.c
@@ -163,7 +163,7 @@ void process_list(struct _GList* list){
     size_t output_size = 0;
     g_list_foreach(list, &print_elem, NULL);
     g_list_foreach(list, &sum_program_length , &output_size);
-    printf("Program length: %ld\n",output_size);
+    printf("Program length: %zu\n",output_size);
     output_size *= 2;
     uint16_t* buffer = malloc(output_size);
     uint16_t* buf_save = buffer;
