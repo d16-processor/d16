60a6f1f86b6c7ab33b0a4ced36e464bba8552fe9
C-Elegans <mtnolan2640@gmail.com>
2016-06-28 10:24:21 -0400

    Add regression tests to automake

--- assembler/d16-tests/regression/tests.py
+++ assembler/d16-tests/regression/tests.py
@@ -1,11 +1,15 @@
+#!/usr/bin/python
 import glob,subprocess,sys,filecmp,os
-asmfiles = glob.glob("*.s")
+path =  os.path.realpath(__file__)
+
+asmfiles = glob.glob(path+"*.s")
+print asmfiles
 for file in asmfiles:
-    code = subprocess.call(["../../d16-asm/d16",file,"mem.bin"], stdout=open(os.devnull, 'wb'))
+    code = subprocess.call([path + "../../d16-asm/d16",path+file,path+"mem.bin"], stdout=open(os.devnull, 'wb'))
     if code:
         sys.exit(1)
     expected = file[:-2] + ".bin"
     actual = "mem.bin"
-    if not filecmp.cmp(actual,expected):
+    if not filecmp.cmp(path+actual,path+expected):
         sys.exit(2)
     print "Test: " + file[:-2] + " passed"
