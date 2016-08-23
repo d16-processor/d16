a816d60863de66fc17fbeed33dea0bf8bb6091f7
C-Elegans <mtnolan2640@gmail.com>
2016-08-23 12:55:45 -0400

    Fix baud clock period (again)

--- vhdl/src/baud_clock.vhd
+++ vhdl/src/baud_clock.vhd
@@ -12,17 +12,17 @@ end entity baud_clock;
 architecture RTL of baud_clock is
 	signal s_baud_clock : std_logic;
 	signal s_baud_counter : unsigned(4 downto 0);
-	signal s_baud_error : std_logic;
+	signal s_baud_error : unsigned(3 downto 0);
 begin
 	name : process(clk) is
 	begin
 		if rising_edge(clk) then
 			if en = '1' then
 				s_baud_counter  <= s_baud_counter + 1;
-				if (s_baud_counter = '1' & X"A" and s_baud_clock = '0' and s_baud_error = '1') or (s_baud_counter = '1' & X"B" and s_baud_clock = '0') then
+				if (s_baud_counter = '1' & X"B" and s_baud_clock = '0' ) or (s_baud_counter = '1' & X"A" and s_baud_clock = '0' and s_baud_error > 3)  then
 					s_baud_clock  <= '1';
 					s_baud_counter  <= '0' & X"0";
-					s_baud_error  <= not s_baud_error ;
+					s_baud_error  <= s_baud_error + 1 ;
 				end if;
 				if s_baud_counter = '1' & X"A" and s_baud_clock = '1' then
 					s_baud_clock  <= '0';
@@ -31,7 +31,7 @@ begin
 			else
 				s_baud_clock <= '0';
 				s_baud_counter  <= "00000";
-				s_baud_error  <= '0';
+				s_baud_error  <= X"0";
 			end if;
 		end if;
 	end process name;
