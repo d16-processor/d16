68e2aeb917149f17b530f33ee78667f438965ddb
C-Elegans <mtnolan2640@gmail.com>
2017-02-08 16:48:41 -0500

    merge with upstream

--- verilog/src/top.v
+++ verilog/src/top.v
@@ -3,8 +3,6 @@ module top(input CLOCK_50, input [1:0] KEY, output [7:0] LED, output Tx, input R
 wire clk;
 wire rst_n;
 
-reg [2:0] rst_sync = 0;
-reg [23:0] counter = 0;
 core core(/*AUTOINST*/
           // Outputs
           .LED                          (LED),
@@ -13,11 +11,6 @@ core core(/*AUTOINST*/
           .clk                          (clk),
           .rst_n                        (rst_n),
           .rx                           (Rx));
-assign clk = counter[21];
-assign rst_n = rst_sync[2];
-always @(posedge CLOCK_50) begin
-    counter <= counter + 1;
-	 rst_sync <= {rst_sync[1:0],KEY[1]};
-end
+assign clk = CLOCK_50;
 
 endmodule
